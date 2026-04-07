9071785cd7162c8bbafbc133bb2cecc7ad832609
Yaswanth Gandhi Vanama (V) <yaswanth_gandhi_vanama@apple.com>
2026-04-07 19:06:09 +0530

    Battery Page Errors

--- Geni-View Cloud/Controllers/BatteriesController.cs
+++ Geni-View Cloud/Controllers/BatteriesController.cs
@@ -353,12 +353,12 @@ namespace GeniView.Cloud.Controllers
             return Json(model);
         }
 
-        public ActionResult GetBatteryDetailData(string id)
+        public ActionResult GetBatteryDetailData(string ID)
         {
             BatteryDetailViewModel model = new BatteryDetailViewModel();
             try
             {
-                model = batterydb.GetBatteryDetails(id);
+                model = batterydb.GetBatteryDetails(ID);
                 ViewBag.StateCount = EnumHelper.GetFriendlyText(model.State).Split('\n').Count() - 1 +
                                      model.LastAgentBatteryLog.OperatingData.BatteryOperatingStatus.StatusAText.Split(',').Count() - 1 +
                                      model.LastAgentBatteryLog.OperatingData.BatteryOperatingStatus.StatusBText.Split(',').Count() - 1;
